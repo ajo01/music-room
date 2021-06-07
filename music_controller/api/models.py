a3c5ebe401ab53320d0d808277084ee22aa5ff32
ajo01 <amyjojjm2001@gmail.com>
2021-06-07 01:14:33 -0700

    resolved votes_to_skip table, rooms web showing

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -15,6 +15,6 @@ class Room(models.Model):
     code = models.CharField(max_length=8, default="", unique=True)
     host = models.CharField(max_length=30, unique=True)
     guest_can_pause = models.BooleanField(null=False, default=False)
-    vote_to_skip = models.IntegerField(null=False, default=1)
+    votes_to_skip = models.IntegerField(null=False, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
 
