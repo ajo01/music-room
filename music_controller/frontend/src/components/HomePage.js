dc49648549f47feb47740bf25c1796c862ad31a1
ajo01 <amyjojjm2001@gmail.com>
2021-06-07 13:29:58 -0700

    django and react routing

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import CreateRoom from "./CreateRoom";
+import RoomJoin from "./RoomJoin";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
 const HomePage = () => {
-    return (
-        <div>Home Page</div>
-    )
-}
+  return (
+      <Router>
+          <Switch>
+              <Route exact path="/">
+                  <p>Home page link</p>
+              </Route>
+              <Route path="/join" component={RoomJoin}/>
+              <Route path="/create" component={CreateRoom}/>
+          </Switch>
+      </Router>
+  );
+};
 
-export default HomePage
+export default HomePage;
