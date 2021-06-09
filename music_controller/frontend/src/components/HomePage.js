a3584bd0d0f717a3536338afc938ffab58960f3d
ajo01 <amyjojjm2001@gmail.com>
2021-06-09 02:47:00 -0700

    homepage styling and routing to join/create

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import CreateRoom from "./CreateRoom";
 import RoomJoin from "./RoomJoin";
 import {
@@ -8,21 +8,40 @@ import {
   Link,
   Redirect,
 } from "react-router-dom";
-import Room from './Room'
+import { Button, Grid, Typography, ButtonGroup } from "@material-ui/core";
+import Room from "./Room";
 
-const HomePage = () => {
-  return (
+export default class HomePage extends Component {
+  renderHomePage() {
+    return (
+      <Grid container spacing={3}>
+        <Grid item xs={12} align="center">
+          <Typography variant="h3" compact="h3">
+            House Party
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <ButtonGroup disableElevation variant="contained" color="primary">
+            <Button color="primary" to="/join" component={Link}>Join a Room</Button>
+            <Button color="secondary" to="/create" component={Link}>Create a Room</Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+    );
+  }
+
+  render() {
+    return (
       <Router>
-          <Switch>
-              <Route exact path="/">
-                  <p>Home page link</p>
-              </Route>
-              <Route path="/join" component={RoomJoin}/>
-              <Route path="/create" component={CreateRoom}/>
-              <Route path="/room/:roomCode" component={Room} />
-          </Switch>
+        <Switch>
+          <Route exact path="/">
+            {this.renderHomePage()}
+          </Route>
+          <Route path="/join" component={RoomJoin} />
+          <Route path="/create" component={CreateRoom} />
+          <Route path="/room/:roomCode" component={Room} />
+        </Switch>
       </Router>
-  );
-};
-
-export default HomePage;
+    );
+  }
+}
