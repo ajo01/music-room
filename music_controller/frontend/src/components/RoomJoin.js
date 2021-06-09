675748ad51a02f2347c1701d84685fffbfee2424
ajo01 <amyjojjm2001@gmail.com>
2021-06-08 20:52:05 -0700

    roomjoin basic styling

--- music_controller/frontend/src/components/RoomJoin.js
+++ music_controller/frontend/src/components/RoomJoin.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, {Component} from "react";
+import {
+  Button,
+  Grid,
+  Typography,
+  TextField
+} from "@material-ui/core";
+import { Link } from "react-router-dom";
 
-const RoomJoin = () => {
-    return (
-        <div>RoomJoin</div>
-    )
-}
+export default class RoomJoin extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          roomCode: "",
+          error: "",
+        };
+    }
 
-export default RoomJoin
+    render() {
+        return (
+            <Grid container spacing={1} alignItems="center">
+                <Grid item xs={12}>
+                    <Typography variant="h4" component="h4">Join A Room</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField error={this.state.error} label="Code" placeholder="Enter a room code" value={this.state.roomCode} helper={this.state.error} variant="outlined"/>
+                </Grid>
+            </Grid>
+        );
+    }
+};
