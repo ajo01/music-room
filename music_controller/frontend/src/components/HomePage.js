536963894af39c3563eca18972effea570bb8892
ajo01 <amyjojjm2001@gmail.com>
2021-06-09 18:57:45 -0700

    room redirects to home if room left

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -17,6 +17,7 @@ export default class HomePage extends Component {
     this.state = {
       roomCode: null,
     };
+    this.clearRoomCode = this.clearRoomCode.bind(this);
   }
 
   async componentDidMount() {
@@ -47,17 +48,36 @@ export default class HomePage extends Component {
     );
   }
 
+  clearRoomCode() {
+    this.setState({
+      roomCode: null,
+    });
+  }
+
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" render={() => {
-            return this.state.roomCode ? (<Redirect to={`/room/${this.state.roomCode}`}></Redirect>) : (this.renderHomePage())
-          }}>
-          </Route>
+        <Route
+            exact
+            path="/"
+            render={() => {
+              return this.state.roomCode ? (
+                <Redirect to={`/room/${this.state.roomCode}`} />
+              ) : (
+                this.renderHomePage()
+              );
+            }}
+          />
+
           <Route path="/join" component={RoomJoin} />
           <Route path="/create" component={CreateRoom} />
-          <Route path="/room/:roomCode" component={Room} />
+          <Route
+            path="/room/:roomCode"
+            render={(props) => {
+              return <Room {...props} leaveRoomCallback={this.clearRoomCode} />;
+            }}
+          />
         </Switch>
       </Router>
     );
