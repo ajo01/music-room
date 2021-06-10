a6e6b3a4b3c657147f37f9ac98ebbc7cd69416d0
ajo01 <amyjojjm2001@gmail.com>
2021-06-10 14:20:44 -0700

    spotify auth url

--- music_controller/frontend/urls.py
+++ music_controller/frontend/urls.py
@@ -1,10 +1,11 @@
 
-from django.urls import path
+from django.urls import path, include
 from .views import index
 
 urlpatterns = [
     path('', index),
     path('join', index),
     path('create', index),
-    path('room/<str:roomCode>', index)
+    path('room/<str:roomCode>', index),
+    path('spotify/', include('spotify.urls'))
 ]
